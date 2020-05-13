de9b6d05736667b7e30a3fb722c0f1575e99472a
Clodoaldo <clodoaldo@CCM-Debian1.CCM>
2020-05-13 01:08:38 -0300

    Build to project management frontend

---
+++ precache-manifest.e37d316c610c0b8446f6a42eec8b6c6d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe36fa13ad14c482aa8fe287cf81e564",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d91f6273599f8a3d29a",
+    "url": "/static/css/main.6570b00f.chunk.css"
+  },
+  {
+    "revision": "ff6528654f60ae86f837",
+    "url": "/static/js/2.5961b937.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5961b937.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d91f6273599f8a3d29a",
+    "url": "/static/js/main.565e44be.chunk.js"
+  },
+  {
+    "revision": "700d31d1c6f37b7bdc51",
+    "url": "/static/js/runtime-main.84578d5e.js"
+  },
+  {
+    "revision": "461f4fa6ee1e63e8a67d5a4f1e8bc0de",
+    "url": "/static/media/enterprise.461f4fa6.bmp"
+  },
+  {
+    "revision": "6273e1e0b4b0c45bff6b3941f0adbc15",
+    "url": "/static/media/project-plan.6273e1e0.bmp"
+  },
+  {
+    "revision": "c24f829160267ec9c0f88026cfdf4a72",
+    "url": "/static/media/project.c24f8291.bmp"
+  },
+  {
+    "revision": "e8eedd5b6f5149e132964acc72547af7",
+    "url": "/static/media/teamwork.e8eedd5b.bmp"
+  }
+]);
